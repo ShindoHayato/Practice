d6cf4b37d925f63e60bf79815127d824830a0f7e
ShindoHayato <shindo.hayato@compass-japan.com>
2025-02-14 10:23:47 +0900

    各ファイルの修正・変更

--- src/main/java/com/example/practice/dto/User.java
+++ src/main/java/com/example/practice/dto/User.java
@@ -1,67 +1,16 @@
 package com.example.practice.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+
+@Getter
 public class User {
     private int id;
     private String mail;
     private String password;
     private String roles;
-    private String created;
-    private String last_logined;
+    private LocalDateTime created;
+    private LocalDateTime last_logined;
     private boolean enabled;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRoles() {
-        return roles;
-    }
-
-    public void setRoles(String roles) {
-        this.roles = roles;
-    }
-
-    public String getCreated() {
-        return created;
-    }
-
-    public void setCreated(String created) {
-        this.created = created;
-    }
-
-    public String getLast_logined() {
-        return last_logined;
-    }
-
-    public void setLast_logined(String last_logined) {
-        this.last_logined = last_logined;
-    }
-
-    public boolean getEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
 }
